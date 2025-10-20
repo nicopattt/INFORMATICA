af0c20c92068d4da6bad89487c942ce6a9c30105
s-nicolo.paternoster@istitutopilati.it <s-nicolo.paternoster@istitutopilati.it>
2025-10-20 12:02:23 +0200

    5

--- C++/Prova1/main.cpp
+++ C++/Prova1/main.cpp
@@ -1,27 +1,7 @@
 #include <iostream>
+#include "Counter.h"
 
 using namespace std;
-
-class Counter {
-
-    private:
-        int value;
-
-    public:
-        Counter(){
-            cout << "istanza dell'oggetto creata\n";
-            value = 0;
-        }
-        ~Counter(){
-            cout << "oggetto distrutto! \n";
-        }
-        int getValue(){
-            return value;
-        }
-        void increase(int n){
-            value+=n;
-        }
-};
 
 int main(){
 
@@ -30,9 +10,8 @@ int main(){
     cout << "Contatore 1: " << c1.getValue() << "\n";
     cout << "Contatore 2: " << c2.getValue() << "\n";
 
-    for(int i=0; i<100; i++){
-        c1.increase(1);
-    }
+    c1.increase(10);
+    c2.increase(20);
 
     cout << "Contatore 1: " << c1.getValue() << "\n";
     cout << "Contatore 2: " << c2.getValue() << "\n";
